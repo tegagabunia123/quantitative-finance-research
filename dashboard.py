323261e2b606fa6ae5a9e827b172cd684a4f4e38
Tega_Gabunia <97608334+tegagabunia123@users.noreply.github.com>
2026-09-04 22:31:40 +0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,10 @@
 import streamlit as st
-import sys
+import matplotlib
 import os
 
-sys.path.append(os.path.abspath("graph-theory-portfolios"))
-sys.path.append(os.path.abspath("qt-macro-liquidity"))
+matplotlib.use("Agg")
 
-st.sidebar.title("Quantitative Research Desk")
+st.sidebar.title("Main Desk")
 
 page = st.sidebar.radio(
     "Select Research Track", 
@@ -13,7 +12,31 @@ page = st.sidebar.radio(
 )
 
 if page == "graph-theory-portfolios":
-    import portfolio_risk_and_return_bar_chart_comparsion.py
+    st.title("Network-Based Portfolio Analytics Matrix")
+    
+    # Target path inside your folder
+    file_path = os.path.join("graph-theory-portfolios", "portfolio_risk_and_return_bar_chart_comparsion.py")
+    
+    if os.path.exists(file_path):
+        with open(file_path, "r", encoding="utf-8") as f:
+            code = f.read()
+        
+        exec(code, {"__name__": "__main__"})
+        
+        st.pyplot(plt.gcf())
+    else:
+        st.error(f"Could not locate file at: {file_path}")
 
 elif page == "qt-macro-liquidity":
-    import fed_balance_sheet_vs_sandp500_and_vix.py
+    st.title("Rolling Correlation: Fed Balance Sheet vs VIX")
+    
+    file_path = os.path.join("qt-macro-liquidity", "fed_balance_sheet_vs_sandp500_and_vix.py.py")
+    
+    if os.path.exists(file_path):
+        with open(file_path, "r", encoding="utf-8") as f:
+            code = f.read()
+            
+        exec(code, {"__name__": "__main__"})
+        st.pyplot(plt.gcf())
+    else:
+        st.error(f"Could not locate file at: {file_path}")
